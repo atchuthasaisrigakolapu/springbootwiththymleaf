6fad74bd8d0876ce82231aa4c69fdede6d43b902
atchuthasaisrigakolapu <atchuthasaisrigakolapu@gmail.com>
2022-04-09 10:03:34 +0530

    Default code changes

---
+++ src/main/java/com/example/springbootwiththymleaf/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.springbootwiththymleaf;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootwiththymleafApplication.class);
+	}
+
+}
